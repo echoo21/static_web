69f61bea705ca9a49d83e9bb0e58e0de9dd80fa1
Richie Hujaya <richie.hujaya2007@gmail.com>
2026-06-25 11:19:42 +0700

    fix github blank page again

--- vite.config.js
+++ vite.config.js
@@ -11,18 +11,6 @@ export default defineConfig({
     cssMinify: true,
     cssCodeSplit: true,
     assetsInlineLimit: 0,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules/react-dom'))  return 'vendor-dom'
-          if (id.includes('node_modules/react'))      return 'vendor-react'
-          if (id.includes('node_modules/axios'))      return 'vendor-http'
-          if (id.includes('node_modules/@headlessui')) return 'vendor-ui'
-          if (id.includes('node_modules/@heroicons')) return 'vendor-icons'
-          if (id.includes('node_modules'))            return 'vendor-other'
-        },
-      },
-    },
     chunkSizeWarningLimit: 300,
   },
 })
